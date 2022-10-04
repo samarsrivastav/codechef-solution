4339cbe708d22bcbbb07dce986a5d2200bbcea7f
Rutvik Aakarsh <88586497+Rutvuku@users.noreply.github.com>
2022-10-04 22:30:54 +0530

    Add files via upload

---
+++ pretty number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int L;
+    int R;
+
+    int y=0;
+
+    cin>>T;
+    for(int i=T ; i>0 ; i--){
+        cin>>L>>R;
+
+
+
+
+
+            for(int k= L ; k<=R ; k++){
+                if(k%10==2 || k%10==3 || k%10==9){
+                    y++;
+                }
+            }cout<<y<<endl;
+
+
+    }
+    return 0;
+}
