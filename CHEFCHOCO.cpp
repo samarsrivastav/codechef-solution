456e7b2b4f74198551e03e4b896ac7b103145474
Lakshya2907 <115323222+Lakshya2907@users.noreply.github.com>
2022-10-08 03:33:34 +0530

    Update CHEFCHOCO.cpp

--- CHEFCHOCO.cpp
+++ CHEFCHOCO.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main(){
- int t{};
+ int t;
  cin>>t;
- for (int i = 1; i <=t; i++)
- {
-   int c{},x{},y{};
-   cin>>c>>x>>y;
-   cout<<(c-x)*y<<endl;
- }
-    return 0;
+ 
+ while(t--){
+  int c,x,y;
+  cin>>c>>x>>y;
+  cout<<(c-x)*y<<endl;
+ } 
+ 
+ return 0;
 }
